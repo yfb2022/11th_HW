119077121d619703f39ed47589e1780843c2e1d0
yfb2022 <a01049248323@gmail.com>
2025-05-23 11:54:52 +0900

    작업환경 세팅

--- 코드_공유.py
+++ 코드_공유.py
@@ -17,13 +17,40 @@ wine = load_wine()
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
 ''' 코드 작성 바랍니다 '''
+wine_df = pd.DataFrame(data=wine.data, columns=wine.feature_names)
+wine_df['target'] = wine.target
+X = wine_df.drop(columns='target')
+y = wine_df['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 ####### A 작업자 작업 수행 #######
 
 ''' 코드 작성 바랍니다 '''
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns
 
+param_grid = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [2, 5],
+    'min_samples_split': [2, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
 
+clf = DecisionTreeClassifier(random_state=42)
+gridsearch = GridSearchCV(clf, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+gridsearch.fit(X_train, y_train)
+print(gridsearch.best_params_)
+
+importances = gridsearch.best_estimator_.feature_importances_
+indices = importances.argsort()[::-1]
+features = X.columns
+plt.figure(figsize=(8, 6))
+plt.title("Feature Importances")
+sns.barplot(x= importances[indices], y= features[indices], palette='viridis')
+plt.ylabel('Feature')
+plt.show()
 
 ####### B 작업자 작업 수행 #######
 
